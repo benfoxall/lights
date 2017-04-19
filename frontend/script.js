664ec58f89dac1d0041299826c80f9e8ec2a8130
Ben Foxall <benfoxall@gmail.com>
2017-04-19 03:06:57 +0100

    initial frontend

---
+++ frontend/script.js
@@ -0,0 +1,13 @@
+for (var i = 0; i < 20; i++) {
+  var led = document.createElement('li')
+  led.style.marginTop = Math.random() + 'em'
+  led.style.transitionDelay = (i/50) + 's'
+  leds.appendChild(led)
+}
+
+document.body.addEventListener('click', function(e){
+  if(e.target.nodeName == 'A') {
+    var color = window.getComputedStyle(e.target).color
+    leds.style.setProperty('--led-color', color)
+  }
+})
