c252f2cc51eacb644b26da8b74733aa2920e21c5
Ben Foxall <benfoxall@gmail.com>
2017-04-21 01:00:14 +0100

    whoop whoop whoop

--- docs/script.js
+++ docs/script.js
@@ -1,3 +1,7 @@
+var lights = null
+var rgbRE = /rgb\((\d+), (\d+), (\d+)/
+
+
 for (var i = 0; i < 20; i++) {
   var led = document.createElement('li')
   led.style.marginTop = Math.random() + 'em'
@@ -9,7 +13,61 @@ document.body.addEventListener('click', function(e){
   if(e.target.nodeName == 'A') {
     var color = window.getComputedStyle(e.target).color
     leds.style.setProperty('--led-color', color)
+
+    //todo: better
+    if(lights) {
+      var parts = color.match(rgbRE);
+      console.log(parts)
+      var buf = Uint8ClampedArray.from([
+        parseInt(parts[1],10),
+        parseInt(parts[2],10),
+        parseInt(parts[3],10)
+      ]).buffer
+
+      lights.writeValue(buf)
+
+    }
+
+
   }
 }, false)
 
 navigator.serviceWorker.register('service-worker.js')
+
+
+
+if(navigator.bluetooth) {
+  connect.style.display = 'block';
+
+  connect.addEventListener('click', () => {
+
+    navigator.bluetooth.requestDevice({
+      filters: [{namePrefix: 'Puck'}],
+      optionalServices: [0xBCDE]
+    })
+    .then(device => device.gatt.connect())
+    .then(server => {
+      return server.getPrimaryService(0xBCDE);
+    })
+    .then(service => {
+      return service.getCharacteristic(0xABCD);
+    })
+    .then(characteristic => {
+      window.characteristic = characteristic
+
+      connect.style.display = 'none'
+
+      lights = characteristic
+
+      var buf = Uint8ClampedArray.from([20]).buffer
+
+      return characteristic.writeValue(buf)
+
+
+    })
+    .then(c => console.log(c))
+
+
+  })
+
+}
