05feb1ca6322ee56c9aa45ec157ca4086252d420
Ben Foxall <benfoxall@gmail.com>
2017-04-29 10:05:11 +0100

    Hook cache version to GitHub build /Props @omgmog

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,4 +1,8 @@
-const CACHE_NAME = 'fairy-lights-2'
+---
+layout: # no layout
+---
+
+const CACHE_NAME = 'fairy-lights-{% if site.github %}{{site.github.build_revision}}{% endif %}'
 
 const urlsToCache = [
   '.',
@@ -12,6 +16,20 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
+  )
+})
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
   )
 })
 
